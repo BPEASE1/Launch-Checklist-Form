01c35c6ce2d2e2ad64ff7213ee9adda4e5a38666
Brian Pease <pease.brian@gmail.com>
2020-12-26 16:18:01 -0600

    message

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+   <li>Name: ${K2-18b}</li>
+   <li>Diameter: ${34500 km}</li>
+   <li>Star: ${K2-18}</li>
+   <li>Distance from Earth: ${110 light years from Earth}</li>
+   <li>Number of Moons: ${unknown}</li>
 </ol>
-<img src="${}">
+<img src="${https://www.nasa.gov/sites/default/files/thumbnails/image/heic1916a.jpg}">
 */
